3691824a124ba6efe990c00920185d6e1515efcb
Andrew <71823699+riezong@users.noreply.github.com>
2025-03-10 12:37:02 +1100

    Create todoItem class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './styles.css';
+
+class todoItem {
+	constructor(title, description, dueDate, priority, notes, checklist) {
+		this.title = title;
+		this.description = description;
+		this.dueDate = dueDate;
+		this.priority = priority;
+		this.notes = notes;
+		this.checklist = checklist;
+	}
+}
+
+const testItem = new todoItem(
+	'study',
+	'study one hour',
+	'today',
+	'1',
+	'The Odin Project',
+	true
+);
+console.log(testItem);
