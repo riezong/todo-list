5c85d5b9564ff630efbbbeb1f2304a90f4d83b91
Andrew <71823699+riezong@users.noreply.github.com>
2025-03-10 18:02:19 +1100

    Render list to DOM

--- src/todo-list-DOM.js
+++ src/todo-list-DOM.js
@@ -1,3 +1,40 @@
-const todoListDOM = (function () {})();
+import { todoListLogic } from './todo-list-logic.js';
+
+const projectList = todoListLogic.projects;
+console.log(projectList);
+
+const todoListDOM = (function () {
+	const Content = document.querySelector('#content');
+
+	const sidebar = (function () {
+		const sidebar = document.createElement('div');
+		sidebar.setAttribute('id', 'sidebar');
+		sidebar.textContent = 'sidebar';
+		Content.appendChild(sidebar);
+	})();
+
+	const projects = (function () {
+		const projects = document.createElement('div');
+		projects.setAttribute('id', 'projects');
+		Content.appendChild(projects);
+
+		const projectTitle = document.createElement('div');
+		// projectTitle.setAttribute('id', 'projects');
+		projectTitle.textContent = 'projects';
+		projects.appendChild(projectTitle);
+
+		const projectLists = document.createElement('div');
+		projectLists.setAttribute('id', 'project-list');
+		projects.appendChild(projectLists);
+		const projectBody = document.createElement('div');
+		for (const list of projectList) {
+			const newList = document.createElement('div');
+			newList.setAttribute('id', list.title);
+			newList.setAttribute('class', 'todo-list');
+			newList.textContent = list.title;
+			projectLists.appendChild(newList);
+		}
+	})();
+})();
 
 export { todoListDOM };
--- src/todo-list-logic.js
+++ src/todo-list-logic.js
@@ -1,53 +1,59 @@
-const todoListLogic = (function() {
+const todoListLogic = (function () {
+	const projects = [];
 
-const projects = [];
+	class todoList {
+		constructor(title) {
+			this.title = title;
+			this.todoList = [];
+			projects.push(this); // adds that object to the projects array. 'this' refers to the newly created todoList object.
+		}
 
-class todoList {
-	constructor(title) {
-		this.title = title;
-		this.todoList = [];
-		projects.push(this); // adds that object to the projects array. 'this' refers to the newly created todoList object.
+		add(todoItem) {
+			this.todoList.push(todoItem);
+		}
+
+		getTitle() {
+			return this.title;
+		}
+
+		getTodos() {
+			return this.todoList;
+		}
 	}
 
-	add(todoItem) {
-		this.todoList.push(todoItem);
+	const listTodo = new todoList('todo');
+	const listDoing = new todoList('doing');
+	const listDone = new todoList('done');
+	// console.log(listTodo, typeof listTodo);
+	// console.log(listDoing, typeof listDoing);
+	// console.log(listDone, typeof listDone);
+	// console.log(projects);
+
+	class todoItem {
+		constructor(title, description, dueDate, priority, notes, checklist) {
+			this.title = title; // string
+			this.description = description; // string
+			this.dueDate = new Date(dueDate); // date
+			this.priority = priority; // number
+			this.notes = notes; // string
+			this.checklist = checklist; // array? boolean?
+		}
 	}
 
-	getTitle() {
-		return this.title;
-	}
+	const testItem = new todoItem(
+		'study',
+		'study one hour',
+		'today',
+		1,
+		'The Odin Project',
+		true
+	);
 
-	getTodos() {
-		return this.todoList;
-	}
-}
+	listTodo.add(testItem);
 
-const defaultList = new todoList('default');
-console.log(projects, typeof projects);
-// console.log(defaultList, typeof defaultList);
-
-class todoItem {
-	constructor(title, description, dueDate, priority, notes, checklist) {
-		this.title = title; // string
-		this.description = description; // string
-		this.dueDate = new Date (dueDate); // date
-		this.priority = priority; // number
-		this.notes = notes; // string
-		this.checklist = checklist; // array? boolean?
-	}
-}
-
-const testItem = new todoItem(
-	'study',
-	'study one hour',
-	'today',
-	1,
-	'The Odin Project',
-	true
-);
-
-defaultList.add(testItem);
-    
+	return { projects };
 })();
+
+console.log(todoListLogic.projects);
 
 export { todoListLogic };
