79421119e1c0790216495096ea4493992b60ee1e
Andrew <71823699+riezong@users.noreply.github.com>
2025-03-11 16:19:23 +1100

    Render items to DOM

--- src/todo-list-DOM.js
+++ src/todo-list-DOM.js
@@ -1,7 +1,9 @@
 import { todoListLogic } from './todo-list-logic.js';
 
 const projectList = todoListLogic.projects;
-console.log(projectList);
+// console.log(projectList[0]);
+// console.log(listTodo, typeof listTodo);
+// console.log(listTodo.getTodos());
 
 const todoListDOM = (function () {
 	const Content = document.querySelector('#content');
@@ -28,10 +30,48 @@ const todoListDOM = (function () {
 		projects.appendChild(projectLists);
 		const projectBody = document.createElement('div');
 		for (const list of projectList) {
+			// console.log(list, typeof list);
+			// console.log(list.getTodos(), typeof list.getTodos());
+			const todos = list.getTodos();
+			// console.log(todos, typeof todos);
+
 			const newList = document.createElement('div');
 			newList.setAttribute('id', list.title);
 			newList.setAttribute('class', 'todo-list');
 			newList.textContent = list.title;
+
+			if (todos.length > 0) {
+				for (const item of todos) {
+					console.log(item);
+
+					const newItem = document.createElement('div');
+					newItem.setAttribute('id', item.title);
+					newItem.setAttribute('class', 'todo-item');
+
+					const itemTitle = document.createElement('h1');
+					itemTitle.textContent = item.title;
+					newItem.appendChild(itemTitle);
+
+					const itemDescription = document.createElement('h2');
+					itemDescription.textContent = item.description;
+					newItem.appendChild(itemDescription);
+
+					const itemDue = document.createElement('p');
+					itemDue.textContent = item.dueDate;
+					newItem.appendChild(itemDue);
+
+					const itemPriority = document.createElement('p');
+					itemPriority.textContent = item.priority;
+					newItem.appendChild(itemPriority);
+
+					const itemNotes = document.createElement('p');
+					itemNotes.textContent = item.notes;
+					newItem.appendChild(itemNotes);
+
+					newList.appendChild(newItem);
+				}
+			}
+
 			projectLists.appendChild(newList);
 		}
 	})();
--- src/todo-list-logic.js
+++ src/todo-list-logic.js
@@ -1,3 +1,5 @@
+import { compareAsc, format } from 'date-fns';
+
 const todoListLogic = (function () {
 	const projects = [];
 
@@ -33,27 +35,59 @@ const todoListLogic = (function () {
 		constructor(title, description, dueDate, priority, notes, checklist) {
 			this.title = title; // string
 			this.description = description; // string
-			this.dueDate = new Date(dueDate); // date
+			this.dueDate = format(new Date(dueDate), 'dd.MM.yyyy'); // date
 			this.priority = priority; // number
 			this.notes = notes; // string
 			this.checklist = checklist; // array? boolean?
 		}
 	}
 
-	const testItem = new todoItem(
+	const todoTestItem = new todoItem(
 		'study',
 		'study one hour',
-		'today',
+		'2025-03-10',
 		1,
 		'The Odin Project',
 		true
 	);
+	listTodo.add(todoTestItem);
 
-	listTodo.add(testItem);
+	const todoTestItem2 = new todoItem(
+		'exercise',
+		'walk 5000 steps',
+		'2025-03-10',
+		1,
+		'The Odin Project',
+		true
+	);
+	listTodo.add(todoTestItem2);
+
+	const doingTestItem = new todoItem(
+		'exercise',
+		'walk 5000 steps',
+		'2025-03-10',
+		1,
+		'The Odin Project',
+		true
+	);
+	listDoing.add(doingTestItem);
+
+	const doneTestItem = new todoItem(
+		'exercise',
+		'walk 5000 steps',
+		'2025-03-10',
+		1,
+		'The Odin Project',
+		true
+	);
+	listDone.add(doneTestItem);
+
+	// console.log(listTodo, typeof listTodo);
+	// console.log(listTodo.getTodos());
 
 	return { projects };
 })();
 
-console.log(todoListLogic.projects);
+// console.log(todoListLogic.projects);
 
 export { todoListLogic };
